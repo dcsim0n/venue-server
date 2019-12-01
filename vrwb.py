89a6d8b1cf6bf7bb5e9f6e4cbca1502118350688
Dana Simmons <dsanalog@gmail.com>
2019-12-01 14:11:49 -0500

    VRWB passes battery type and serial tests

--- vrwb.py
+++ vrwb.py
@@ -8,14 +8,14 @@ class VRWB(DeviceABC):
 
         print("Creating VRWB device")
         
-    _data = {
+    _data = { 
             'channels':[
-                {'block': 21, 'freq': 200, 'label': 'none'},
-                {'block': 21, 'freq': 205, 'label': 'none'},
-                {'block': 21, 'freq': 206, 'label': 'none'},
-                {'block': 21, 'freq': 208, 'label': 'none'},
-                {'block': 21, 'freq': 210, 'label': 'none'},
-                {'block': 21, 'freq': 214, 'label': 'none'},
+                {'block': '23', 'freq': 200, 'label': 'none', 'bat_type': '4', 'voltage': '128' },
+                {'block': '25', 'freq': 205, 'label': 'none', 'bat_type': '4', 'voltage': '128' },
+                {'block': '23', 'freq': 206, 'label': 'none', 'bat_type': '4', 'voltage': '128' },
+                {'block': '26', 'freq': 208, 'label': 'none', 'bat_type': '0', 'voltage': '128' },
+                {'block': '23', 'freq': 210, 'label': 'none', 'bat_type': '0', 'voltage': '128' },
+                {'block': '23', 'freq': 214, 'label': 'none', 'bat_type': '0', 'voltage': '128' },
             ],
             'type': 'VRWB',
             'serial': '123456'
@@ -23,11 +23,20 @@ class VRWB(DeviceABC):
 
     def block(self, args):
         print('parsing:' + str(args) )
-        return 'Data..\n'
+        blocks = map(lambda chan: chan['block'], self._data['channels']) 
+
+        return '{' + ','.join( blocks ) + '}'
     
+    def txbatt(self, args):
+        bat_types = map(lambda chan: chan['bat_type'], self._data['channels'])
+        return '{' + ','.join( bat_types ) + '}'
+
     def id(self, args):
         print('pasing:' + str(args))
         return '"' + self._data['type'] + '"'
+    
+    def serial(self, args):
+        return '"620023"'
 
 
 if __name__ == "__main__":
