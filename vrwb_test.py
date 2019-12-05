494c4e6045f480b1ec9adeead6cf531042c719fd
Dana Simmons <dsanalog@gmail.com>
2019-12-04 21:43:23 -0500

    Stub out vrwb tests

--- vrwb_test.py
+++ vrwb_test.py
@@ -16,14 +16,27 @@ def test_block():
     blocks = sendMsg( b'block(*) ?\r' )
     assert blocks == b'OK {23,25,23,26,23,23}\r\n'
 
+def test_bvolts():
+    bvolts = sendMsg( b'bvolts(*) ?\r' )
+    assert bvolts == b'OK {128,128,128,128,128,128}'
+
+def test_bat():
+    bat = sendMsg(b'txbatt(*) ?\r') 
+    assert bat == b'OK {4,4,4,0,0,0}\r\n'
+def test_level():
+    levels = sendMsg(b'level(*) ?\r')
+    assert levels == b'OK {0,0,0,0,0,0}'
+
+def test_signal():
+    pass
+def test_mhz():
+    pass
+
 def test_id():
     id = sendMsg( b'id ?\r' )
     assert id == b'OK "VRWB"\r\n'
 
 
-def test_bat():
-    bat = sendMsg(b'txbatt(*) ?\r') 
-    assert bat == b'OK {4,4,4,0,0,0}\r\n'
 
 def test_rxscan():
     data = sendMsg(b'rxscan(2)=1\r')
