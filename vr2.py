3a65be22bd2c174aaa85f75830aa8d105c583b5c
Dana Simmons <dsanalog@gmail.com>
2019-12-13 11:09:16 -0500

    Move scan logic into DeviceServer
    decided that scan logic will be similar across all devices,
    moved logic for this into DeviceServer class.
    
    Updated several dictionary keys to match changes in DeviceServer

--- vr2.py
+++ vr2.py
@@ -14,7 +14,7 @@ class VR2( DeviceABC ):
         #parse args to get channel
         channel = re.search(r"\((\d)\)\?", args).group(1)
         ch_idx = int( channel ) - 1 #convert to int and subract one for index
-        return '"' + self._data['channels'][ ch_idx ].label + '"'
+        return '"' + self._data['channels'][ ch_idx ]['rx_name'] + '"'
         
     def txblevel(self, args): # battery volts
         volts = map(lambda chan: chan['voltage'], self._data['channels'])
@@ -40,29 +40,22 @@ class VR2( DeviceABC ):
         return '"' + self._data['type'] + '"'
 
     def serial(self, args):
-        return '"' + self._data['serial'] + '"'
+        return '"' + self._data['serial'] + '"'      
 
     def rxscan(self, args):
         m = re.match(r'\(([1-6])\)=([01])',args) # match the channel 
-        channel = m.group(1)
-        toggle = m.group(2)
+        channel = int( m.group(1) )
+        toggle = int ( m.group(2) )
         self.server.toggle_scan_status(channel,toggle)
         #else:
         #    m = re.match(r'\([1-6\*]\)\?$', args) # match for (1)? and (*)? type requests
         # 
         return ''
     def pollsd(self, args):
-        m = re.match(r'\(([1-6])\?\$)',args)
-        channel = m.group(1)
-        curr_offset = self.server._device_data['channels']['offset_idx']
-        next_offset = curr_offset + 14
-        self.server._device_data['channels']['offset_idx'] = next_offset
-        chunk = self.server._device_data['channels']['data']
-        # Build response block
-        # Ok status
-        # 264 chars of dummy data
-        # 4 chars of offset data
-        # windowed scan data
+        m = re.match(r'\(([1-6])\)\?\$',args)
+        channel = int( m.group(1) )
+        
+        return self.server.get_scan_chunk( channel )
 
 
 
